f4475c1ac50421f0641038aa846cfbefb8688531
Querela <ek.92@arcor.de>
2015-04-13 04:47:17 +0200

    Update changes

--- Java/WCMBookServer/src/main/java/de/uni_leipzig/wcmprak/books/wcmbookserver/serve/StartEmbeddedServer.java
+++ Java/WCMBookServer/src/main/java/de/uni_leipzig/wcmprak/books/wcmbookserver/serve/StartEmbeddedServer.java
@@ -1,14 +1,25 @@
 package de.uni_leipzig.wcmprak.books.wcmbookserver.serve;
 
+import org.glassfish.grizzly.http.server.CLStaticHttpHandler;
 import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.grizzly.http.server.StaticHttpHandler;
+import org.glassfish.grizzly.servlet.FilterRegistration;
+import org.glassfish.grizzly.servlet.ServletRegistration;
+import org.glassfish.grizzly.servlet.WebappContext;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.DispatcherType;
 import javax.ws.rs.core.UriBuilder;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.EnumSet;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -18,7 +29,8 @@ public class StartEmbeddedServer {
     private final static Logger log = LoggerFactory.getLogger(StartEmbeddedServer.class);
 
     // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://127.0.0.1:8080/wcmbook";
+    public static final String BASE_URI = getBaseURI().toString();
+    public static final String JERSEY_SERVLET_CONTEXT_PATH = "";
 
     protected static URI getBaseURI() {
         return UriBuilder
@@ -33,9 +45,32 @@ public class StartEmbeddedServer {
                 .packages("de.uni_leipzig.wcmprak.books.wcmbookserver.serve.resources");
     }
 
+    protected static WebappContext getWebappContext() {
+        WebappContext webappContext = new WebappContext("Grizzly WebappContext", JERSEY_SERVLET_CONTEXT_PATH);
+
+        return webappContext;
+    }
+
     protected static HttpServer createServer() {
         // Build grizzly httpServer with jersey/jax-rs resources
         final HttpServer server = GrizzlyHttpServerFactory.createHttpServer(getBaseURI(), getResources(), false);
+
+        // Build webapp context for registering additional servlets
+        WebappContext webappContext = getWebappContext();
+        webappContext.deploy(server);
+
+        /*
+        try {
+            server.getServerConfiguration()
+                    .addHttpHandler(
+                            new CLStaticHttpHandler(
+                                    new URLClassLoader(
+                                            new URL[]{new URL("file:///D/Documents/Universität/WCM-Praktikum/wcm_buch/Java/WCMBookServer/target/WCMBookServer-javadoc.jar")}
+                                    )), "/");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        //*/
 
         return server;
     }
@@ -48,12 +83,14 @@ public class StartEmbeddedServer {
     public static void main(String[] args) throws Exception {
         log.info("Start embedded server for [WCMBookServer] ...");
 
+        // Build & configure server
         final HttpServer server = createServer();
 
         try {
+            // Start server
             server.start();
 
-            // register shutdown hook
+            // Register shutdown hook
             Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
                 @Override
                 public void run() {
@@ -62,8 +99,10 @@ public class StartEmbeddedServer {
                 }
             }, "shutdownHook-server"));
 
+            // Wait for ENTER OR CTRL^C
             log.info("Press ENTER or CTRL^C to exit ...");
             System.in.read();
+            // Stop server
             server.shutdown(3, TimeUnit.SECONDS);
         } catch (IOException ioex) {
             log.error("IO Exception while configuring and starting the server ...", ioex);
