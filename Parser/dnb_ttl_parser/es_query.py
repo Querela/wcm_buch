f31950ba696cd6ad5308744efddeec3693c3b3d3
Lani Lin <yingchilin@gmail.com>
2015-04-26 23:01:03 +0200

    add es_query.py

---
+++ Parser/dnb_ttl_parser/es_query.py
@@ -0,0 +1,43 @@
+import urllib.request as urlre
+import re,json
+
+endpoint = 'http://localhost:9200/dnb_ger'
+
+book_title = 'Harry Potter und der Feuerkelch'
+dQuotes = '\".*\"'
+header = re.compile(r'original_titel')
+
+def build_q_string(in_string):
+    q_words = in_string.split()
+    q_string = '+'.join(q_words)
+    return q_string
+
+def extract_original_title(field):
+    m = re.search(dQuotes, field)
+    original_title = m.group()[1:-1]
+    return original_title
+
+query = '_search?q=title:'+build_q_string(book_title)+'&size=1&fields:original_title'
+
+url_list = [endpoint, query]
+
+my_url = '/'.join(url_list)
+
+print('QUERY = ', my_url)
+
+out = urlre.urlopen(my_url)
+data = out.read()
+print ('OUTPUT = ',data)
+
+# fields = data.split('\n')
+# print('splitted_data = ', fields)
+# if header in str(data):
+#     original_title =extract_original_title(data)
+#     print(original_title)
+
+
+# data_json = json.loads(data)
+# print(data_json['hits']['total'])
+
+
+
